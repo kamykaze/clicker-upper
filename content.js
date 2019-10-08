e951003701e1e58b4a7dcad3ff3a0d11c4365026
Kam Kuo <developer.kam@gmail.com>
2019-10-08 16:17:27 -0700

    added wide 'me mode' in box view

--- content.js
+++ content.js
@@ -3,6 +3,7 @@ $(function() {
     $('body').addClass('visible-status');
     $('body').addClass('clickup-theme');
     $('body').addClass('dash4-colored-faves');
+    $('body').addClass('wide-box-mode');
 
     const log_styles = [
         'color: green',
